f17bf8e1698047ddaf5edf8ab45a2c4a95e9b533
genesis-algorithms <genesisalgorithms@gmail.com>
2019-08-24 15:37:41 +0300

    Updates

---
+++ precache-manifest.bfc58f14d64ec0c4e6a9d88a8aa4cebb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c620291541b15135c9e72f24084162d7",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "8868d17f91c05261a7e4",
+    "url": "/react-tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "1705cd0043d4780a8fbc",
+    "url": "/react-tic-tac-toe/static/js/2.885b1210.chunk.js"
+  },
+  {
+    "revision": "8868d17f91c05261a7e4",
+    "url": "/react-tic-tac-toe/static/js/main.ceaf6905.chunk.js"
+  },
+  {
+    "revision": "2bcf2a783ddcc29874ed",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.de227c7b.js"
+  }
+]);
